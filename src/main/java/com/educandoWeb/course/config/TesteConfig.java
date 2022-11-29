74bb36fda5a237d183e2f1e67957b0fa0bc74175
Elves Aguiar <elvesmhs@hotmail.com>
2022-11-28 22:24:56 -0300

    Category

--- src/main/java/com/educandoWeb/course/config/TesteConfig.java
+++ src/main/java/com/educandoWeb/course/config/TesteConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoWeb.course.entities.Category;
 import com.educandoWeb.course.entities.Order;
 import com.educandoWeb.course.entities.User;
 import com.educandoWeb.course.entities.enums.OrderStatus;
+import com.educandoWeb.course.repositories.CategoryRepository;
 import com.educandoWeb.course.repositories.OrderRepository;
 import com.educandoWeb.course.repositories.UserRepository;
 
@@ -22,6 +24,9 @@ public class TesteConfig implements CommandLineRunner {
 	private UserRepository userRepository;
 	@Autowired
 	private OrderRepository orderRepository;
+	@Autowired
+	private CategoryRepository CategoryRepository;
+
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -32,9 +37,15 @@ public class TesteConfig implements CommandLineRunner {
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WATTING_PAYMENT);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WATTING_PAYMENT); 
-		 
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		CategoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3)); 
 		
 	}
 	
